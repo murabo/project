db2dd4d118d4d4b43f3875fccf08e50bace09ed1
tsukasa-hashimoto <tsukasa.hashimoto@gu3.co.jp>
2013-06-14 13:04:19 +0900

    いったん削除

--- making_modules/Strategy/comparator/Comparator.py
+++
@@ -1,6 +0,0 @@
-#coding:utf8
-class Comparator:
-    def __init__(self):
-        pass
-    def compare(self, h1, h2):
-        self.compare(self, h1, h2)
--- making_modules/Strategy/human.py
+++
@@ -1,8 +0,0 @@
-#coding:utf-8
-class Human:
-    def __init__(self, name, height, weight, age):
-        self.name = name
-        self.height = height
-        self.weight = weight
-        self.age = age
-    

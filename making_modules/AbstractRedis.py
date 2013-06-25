8975a6c3c00e509bbf3222c483a9c158abfd5b1f
tsukasa.hashimoto <tsukasa.hashimoto.1113@gmail.com>
2013-06-25 21:42:27 +0900

    rename

--- making_modules/AbstractRedis.py
+++ making_modules/AbstractRedis.py
@@ -1,76 +1,128 @@
-#coding:utf-8
-
+# -*- coding: utf-8 -*-
+"""
+AttributeKVSのredis versionのようなもの
+AttributeKVSはsetした時点で保存されるが、
+AttributeRedisはsaveを読んだ時にredisに保存される。
+使い方は、tests/test_attribute.pyを参照
+"""
 from __future__ import with_statement
+import copy
+
+import msgpack
+import redis
 import gredis
 
 
-'''
-redisを仕様するための基本的なメソッドを搭載
-'''
-
 class AbstractRedis(object):
-    def __init__(self):
-       self.redis = gredis.get()
 
-    '''
-    文字列型
-    '''
-    def set(self, key, value):
-        self.redis.set(key, value)
+    db_name = 'default'
+    try_count = 1000
+    attributes = {}
 
-    def get(self, key):
-        return self.redis.get(key)
+    class SaveError(Exception):
+        def __init__(self, key):
+            self.key = key
 
-    '''
-    list型
-    '''
-    def append(self, key, values):
-        if isinstance(values, list):
-            with self.redis.pipeline() as pipe:
-                pipe.multi()
-                for value in values:
-                    pipe.rpush(key, value)
-                pipe.execute()
+    def __init__(self, **kwargs):
+        self.db_name = kwargs.get('db_name', None) or self.db_name
+       # self.redis = gredis.get()
+        self.redis = gredis.get(self.db_name)
+
+        self.key = ':'.join([self._key_prefix(), self.get_kvs_key(**kwargs)])
+
+        data = self.redis.get(self.key)
+        self._attributes = msgpack.unpackb(data) if data else copy.copy(self.attributes)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if exc_type:
+            return False
+        self.save()
+        return True
+
+    def __del__(self):
+        pass
+
+    def __getattr__(self, name):
+        if name in self.attributes:
+            return self.__dict__['_attributes'][name]
         else:
-            self.redis.rpush(key, values)
+            return self.__dict__[name]
 
-    def leftappend(self, key, values):
-        if isinstance(values, list):
-            with self.redis.pipeline() as pipe:
-                pipe.multi()
-                for value in values:
-                    pipe.lpush(key, value)
-                pipe.execute()
+    def __setattr__(self, name, value):
+        if name in self.attributes:
+            self.__dict__['_attributes'][name] = value
         else:
-            self.redis.lpush(key, values)
+            self.__dict__[name] = value
 
-    def get_list(self, key, index=0, limit=-1):
-        return self.redis.lrange(key, index, limit)
+    def _key_prefix(self):
+        return self.__class__.__name__
 
-    def get_list_asc(self, key):
-        return self.redis.sort(key, alpha=True)
+    def get_kvs_key(self, **kwargs):
+        return ''
 
-    def get_list_desc(self, key):
-        return self.redis.sort(key, alpha=True, desc=True)
+    def save(self):
+        def f():
+            return self._save()
 
-    '''
-    dict型
-    '''
-    def set_dict(self, key, dict):
-        self.redis.hmset(key, dict)
+        def e():
+            raise self.SaveError(self.key)
 
-    def get_dict(self, key):
-        return self.redis.hgetall(key)
+        return self._transaction_loop(self.try_count, f, e)
 
-    def get_keys(self, key):
-        return self.redis.hkeys(key)
+    def _save(self):
+        with self.redis.pipeline() as pipe:
+            pipe.watch(self.key)
 
-    def get_values(self, key):
-        return self.redis.hvals(key)
+            save_data = pipe.get(self.key)
+            save_data = self._attributes
 
-    '''
-    その他
-    '''
-    def delete(self, key):
-        self.redis.delete(key)
+            pipe.multi()
 
+            pipe.set(self.key, msgpack.packb(save_data))
+
+            pipe.execute()
+        return True
+
+    def _transaction_loop(self, count, f, e):
+        try_count = 0
+        while True:
+            try:
+                try_count += 1
+                result = f()
+                break
+            except redis.exceptions.WatchError:
+                if count < try_count:
+                    e()
+        return result
+
+    def delete(self):
+        """
+        任意のキーをひとつ削除
+        """
+        if not self.redis:
+            self.redis = gredis.get(self.db_name)
+        self.redis.delete(self.key)
+
+    @classmethod
+    def delete_all(cls, db_name='default'):
+        """
+        任意の子クラスが所持している全キーと値を消す
+        params: db_name <string> settingsに書いている名前
+        """
+        key = ':'.join([cls.__name__, '*'])
+        r = gredis.get(db_name)
+        keys = r.keys(key)
+        with r.pipeline(transaction=False) as pipe:
+            for key in keys:
+                pipe.delete(key)
+            pipe.execute()
+
+    def init_values(self):
+        """
+        値の初期化
+        """
+        for key,value in self._attributes.iteritems():
+            self.__dict__['_attributes'][key] = value
---
+++ making_modules/RedisAPI.py
@@ -0,0 +1,76 @@
+#coding:utf-8
+
+from __future__ import with_statement
+import gredis
+
+
+'''
+redisを仕様するための基本的なメソッドを搭載
+'''
+
+class RedisAPI(object):
+    def __init__(self):
+       self.redis = gredis.get()
+
+    '''
+    文字列型
+    '''
+    def set(self, key, value):
+        self.redis.set(key, value)
+
+    def get(self, key):
+        return self.redis.get(key)
+
+    '''
+    list型
+    '''
+    def append(self, key, values):
+        if isinstance(values, list):
+            with self.redis.pipeline() as pipe:
+                pipe.multi()
+                for value in values:
+                    pipe.rpush(key, value)
+                pipe.execute()
+        else:
+            self.redis.rpush(key, values)
+
+    def leftappend(self, key, values):
+        if isinstance(values, list):
+            with self.redis.pipeline() as pipe:
+                pipe.multi()
+                for value in values:
+                    pipe.lpush(key, value)
+                pipe.execute()
+        else:
+            self.redis.lpush(key, values)
+
+    def get_list(self, key, index=0, limit=-1):
+        return self.redis.lrange(key, index, limit)
+
+    def get_list_asc(self, key):
+        return self.redis.sort(key, alpha=True)
+
+    def get_list_desc(self, key):
+        return self.redis.sort(key, alpha=True, desc=True)
+
+    '''
+    dict型
+    '''
+    def set_dict(self, key, dict):
+        self.redis.hmset(key, dict)
+
+    def get_dict(self, key):
+        return self.redis.hgetall(key)
+
+    def get_keys(self, key):
+        return self.redis.hkeys(key)
+
+    def get_values(self, key):
+        return self.redis.hvals(key)
+
+    '''
+    その他
+    '''
+    def delete(self, key):
+        self.redis.delete(key)
+

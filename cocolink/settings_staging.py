7ff376114ba811e9e17644724c240650627844b7
Shinya Murakami <shinya.murakami@gu3.co.jp>
2013-09-06 17:55:11 +0900

    settings

--- cocolink/settings_staging.py
+++ cocolink/settings_staging.py
@@ -20,7 +20,7 @@ DATABASES = {
         'NAME': 'cocolink',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'mrk31229',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }

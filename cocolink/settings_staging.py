edecb373be2c419912e00aea10795ac33309eb04
Shinya Murakami <shinya.murakami@gu3.co.jp>
2013-09-08 16:48:06 +0900

    settings

--- cocolink/settings_staging.py
+++ cocolink/settings_staging.py
@@ -126,7 +126,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    'cocolink/template',
+    ROOT_PATH + 'cocolink/template',
 )
 
 INSTALLED_APPS = (
